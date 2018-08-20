a40e8bca07b050ec7cec83b9496eb63964f3a8ed
TwlSweet <530449639@qq.com>
2018-08-20 10:16:21 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -9,36 +9,36 @@
 </template>
 
 <script>
-import TheHeader from '@/components/layouts/TheHeader'
-import TheFooter from '@/components/layouts/TheFooter'
+  import TheHeader from '@/components/layouts/TheHeader'
+  import TheFooter from '@/components/layouts/TheFooter'
 
-export default {
-  name: 'App',
-  components: {
-    TheHeader,
-    // 注册 TheFooter
-    TheFooter
+  export default {
+    name: 'App',
+    components: {
+      TheHeader,
+      // 注册 TheFooter
+      TheFooter
+    }
   }
-}
 
-</script>
+  </script>
 
-<style lang="scss">
-$container-large-desktop: 1200px;
-$btn-primary-bg: #00b5ad;
-$btn-primary-border: #00b5ad;
-$label-primary-bg: #00b5ad;
-$pagination-active-bg: #00b5ad;
-$pagination-active-border: #00b5ad;
-$pagination-color: #00b5ad;
-$input-border-focus: #00b5ad;
-$link-color: #12c4c5;
-$link-hover-color: #22ddde;
-$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
-$fa-font-path: "~font-awesome/fonts/";
+  <style lang="scss">
+  $container-large-desktop: 1200px;
+  $btn-primary-bg: #00b5ad;
+  $btn-primary-border: #00b5ad;
+  $label-primary-bg: #00b5ad;
+  $pagination-active-bg: #00b5ad;
+  $pagination-active-border: #00b5ad;
+  $pagination-color: #00b5ad;
+  $input-border-focus: #00b5ad;
+  $link-color: #12c4c5;
+  $link-hover-color: #22ddde;
+  $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+  $fa-font-path: "~font-awesome/fonts/";
 
-@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
-@import "~font-awesome/scss/font-awesome";
-@import "./styles/main";
-@import "./styles/extra";
+  @import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+  @import "~font-awesome/scss/font-awesome";
+  @import "./styles/main";
+  @import "./styles/extra";
 </style>
